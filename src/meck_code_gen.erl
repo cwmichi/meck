ca073fbbadf8032b638ae2c4ea182b4fbef14d15
Michael Scharrer <michael.scharrer@cordaware.com>
2021-08-12 14:47:54 +0200

    otp23

--- src/meck_code_gen.erl
+++ src/meck_code_gen.erl
@@ -30,16 +30,16 @@
 
 -define(CURRENT_CALL, '$meck_call').
 -define(call(Module, Function, Arguments),
-        {call, ?LINE,
-         {remote, ?LINE, ?atom(Module), ?atom(Function)},
-         Arguments}).
+  {call, ?LINE,
+    {remote, ?LINE, ?atom(Module), ?atom(Function)},
+    Arguments}).
 
 -define(atom(Atom), {atom, ?LINE, Atom}).
 -define(integer(Integer), {integer, ?LINE, Integer}).
 -define(var(Name), {var, ?LINE, Name}).
 -define(attribute(Attribute, Args), {attribute, ?LINE, Attribute, Args}).
 -define(function(Name, Arity, Clauses),
-        {function, ?LINE, Name, Arity, Clauses}).
+  {function, ?LINE, Name, Arity, Clauses}).
 -define(clause(Arguments, Body), {clause, ?LINE, Arguments, [], Body}).
 -define(tuple(Elements), {tuple, ?LINE, Elements}).
 
@@ -48,89 +48,102 @@
 %%%============================================================================
 
 to_forms(Mod, Expects) ->
-    {Exports, Functions} = functions(Mod, Expects),
-    [?attribute(module, Mod)] ++ attributes(Mod) ++ Exports ++ Functions.
+  {Exports, Functions} = functions(Mod, Expects),
+  [?attribute(module, Mod)] ++ attributes(Mod) ++ Exports ++ Functions.
 
 -spec get_current_call() -> {Mod::atom(), Func::atom()}.
 get_current_call() ->
-    get(?CURRENT_CALL).
+  get(?CURRENT_CALL).
 
 %%%============================================================================
 %%% Internal functions
 %%%============================================================================
 
+attribute({Key, _Value}, Attrs)
+  when Key =:= vsn;
+  Key =:= deprecated;
+  Key =:= optional_callbacks;
+  Key =:= dialyzer ->
+  Attrs;
+attribute({Key, Value}, Attrs)
+  when (Key =:= behaviour orelse Key =:= behavior)
+  andalso is_list(Value) ->
+  lists:foldl(fun(Behavior, Acc) -> [?attribute(Key, Behavior) | Acc] end,
+    Attrs, Value);
+attribute({Key, Value}, Attrs) ->
+  [?attribute(Key, Value) | Attrs].
+
 attributes(Mod) ->
-    try
-        [?attribute(Key, Val) || {Key, Val} <-
-            proplists:get_value(attributes, Mod:module_info(), []),
-            Key =/= vsn, Key =/= deprecated]
-    catch
-        error:undef -> []
-    end.
+  try
+    lists:foldl(fun attribute/2, [],
+      proplists:get_value(attributes, Mod:module_info(), []))
+  catch
+    error:undef -> []
+  end.
 
 functions(Mod, Expects) ->
-    dict:fold(fun(Export, Expect, {Exports, Functions}) ->
-        {[?attribute(export, [Export]) | Exports],
-         [func(Mod, Export, Expect) | Functions]}
-              end,
-              {[], []},
-              Expects).
+  dict:fold(fun(Export, Expect, {Exports, Functions}) ->
+    {[?attribute(export, [Export]) | Exports],
+      [func(Mod, Export, Expect) | Functions]}
+            end,
+    {[], []},
+    Expects).
 
 func(Mod, {Func, Arity}, {anon, Arity, Result}) ->
-    case contains_opaque(Result) of
-        true ->
-            func_exec(Mod, Func, Arity);
-        false ->
-            func_native(Mod, Func, Arity, Result)
-    end;
+  case contains_opaque(Result) of
+    true ->
+      func_exec(Mod, Func, Arity);
+    false ->
+      func_native(Mod, Func, Arity, Result)
+  end;
 func(Mod, {Func, Arity}, _Expect) ->
-    func_exec(Mod, Func, Arity).
+  func_exec(Mod, Func, Arity).
 
 func_exec(Mod, Func, Arity) ->
-    Args = args(Arity),
-    ?function(Func, Arity,
-              [?clause(Args,
-                       [?call(?MODULE, exec,
-                              [?call(erlang, self, []),
-                               ?atom(Mod),
-                               ?atom(Func),
-                               list(Args)])])]).
+  Args = args(Arity),
+  ?function(Func, Arity,
+    [?clause(Args,
+      [?call(?MODULE, exec,
+        [?call(erlang, self, []),
+          ?atom(Mod),
+          ?atom(Func),
+          list(Args)])])]).
 
 func_native(Mod, Func, Arity, Result) ->
-    Args = args(Arity),
-    AbsResult = erl_parse:abstract(Result),
-    ?function(
-            Func, Arity,
-            [?clause(
-                    Args,
-                    [?call(gen_server, cast,
-                           [?atom(meck_util:proc_name(Mod)),
-                            ?tuple([?atom(add_history),
-                                    ?tuple([?call(erlang, self, []),
-                                            ?tuple([?atom(Mod), ?atom(Func),
-                                                    list(Args)]),
-                                            AbsResult])])]),
-                     AbsResult])]).
+  Args = args(Arity),
+  AbsResult = erl_parse:abstract(Result),
+  ?function(
+    Func, Arity,
+    [?clause(
+      Args,
+      [?call(gen_server, cast,
+        [?atom(meck_util:proc_name(Mod)),
+          ?tuple([?atom(add_history),
+            ?tuple([?call(erlang, self, []),
+              ?tuple([?atom(Mod), ?atom(Func),
+                list(Args)]),
+              AbsResult])])]),
+        AbsResult])]).
 
 contains_opaque(Term) when is_pid(Term); is_port(Term); is_function(Term);
-    is_reference(Term) ->
-    true;
+  is_reference(Term) ->
+  true;
 contains_opaque(Term) when is_list(Term) ->
-    lists_any(fun contains_opaque/1, Term);
+  lists_any(fun contains_opaque/1, Term);
 contains_opaque(Term) when is_tuple(Term) ->
-    lists_any(fun contains_opaque/1, tuple_to_list(Term));
+  lists_any(fun contains_opaque/1, tuple_to_list(Term));
 contains_opaque(_Term) ->
-    false.
+  false.
 
 %% based on lists.erl but accepts improper lists.
 lists_any(Pred, []) when is_function(Pred, 1) -> false;
 lists_any(Pred, [Hd|Tail]) ->
-    case Pred(Hd) of
-        true -> true;
-        false -> lists_any(Pred, Tail)
-    end;
+  case Pred(Hd) of
+    true -> true;
+    false -> lists_any(Pred, Tail)
+  end;
 lists_any(Pred, Improper) ->
-    Pred(Improper).
+  Pred(Improper).
 
 args(0)     -> [];
 args(Arity) -> [?var(var_name(N)) || N <- lists:seq(1, Arity)].
@@ -142,58 +155,91 @@ var_name(A) -> list_to_atom("A"++integer_to_list(A)).
 
 %% @hidden
 -spec exec(CallerPid::pid(), Mod::atom(), Func::atom(), Args::[any()]) ->
-        Result::any().
+  Result::any().
 exec(Pid, Mod, Func, Args) ->
-    case meck_proc:get_result_spec(Mod, Func, Args) of
-        undefined ->
-            meck_proc:invalidate(Mod),
-            raise(Pid, Mod, Func, Args, error, function_clause);
-        ResultSpec ->
-            put(?CURRENT_CALL, {Mod, Func}),
-            try
-                Result = meck_ret_spec:eval_result(Mod, Func, Args, ResultSpec),
-                meck_proc:add_history(Mod, Pid, Func, Args, Result),
-                Result
-            catch
-                Class:Reason ->
-                    handle_exception(Pid, Mod, Func, Args, Class, Reason)
-            after
-                erase(?CURRENT_CALL)
-            end
-    end.
+  try meck_proc:get_result_spec(Mod, Func, Args) of
+    undefined ->
+      meck_proc:invalidate(Mod),
+      raise(Pid, Mod, Func, Args, error, function_clause, []);
+    ResultSpec ->
+      eval(Pid, Mod, Func, Args, ResultSpec)
+  catch
+    error:{not_mocked, Mod} ->
+      apply(Mod, Func, Args)
+  end.
+
+%% workaround for the removal of erlang:get_stacktrace/0 beginning
+%% in Erlang 21.
+%% There is no good solution if you want BOTH
+%% a) be able to compile with both pre- and post-21 compilers
+%% b) no warning (e.g. if you use warnings_as_errors)
+%% this is one of(?) the less bad hacks.
+%% `OTP_RELEASE' was introduced in 21, so if it is defined, we use post-21
+%% behaviour, otherwise pre-21.
+%% Note that in the EXCEPTION macro, you can match on Class and Reason,
+%% but not on StackToken. I.e. this works;
+%% ?EXCEPTION(throw,R,S)
+%% but not this;
+%% ?EXCEPTION(C,R,[])
+%% StackToken is used to make the macros hygienic (i.e. to not leak variable
+%% bindings)
+-ifdef(OTP_RELEASE). %% this implies 21 or higher
+-define(EXCEPTION(Class, Reason, StackToken), Class:Reason:StackToken).
+-define(GET_STACK(StackToken), StackToken).
+-else.
+-define(EXCEPTION(Class, Reason, _), Class:Reason).
+-define(GET_STACK(_), erlang:get_stacktrace()).
+-endif.
+
+-spec eval(Pid::pid(), Mod::atom(), Func::atom(), Args::[any()],
+    ResultSpec::any()) -> Result::any() | no_return().
+eval(Pid, Mod, Func, Args, ResultSpec) ->
+  put(?CURRENT_CALL, {Mod, Func}),
+  try
+    Result = meck_ret_spec:eval_result(Mod, Func, Args, ResultSpec),
+    meck_proc:add_history(Mod, Pid, Func, Args, Result),
+    Result
+  catch
+    ?EXCEPTION(Class, Reason, StackToken) ->
+      handle_exception(Pid, Mod, Func, Args,
+        Class, Reason, ?GET_STACK(StackToken))
+  after
+    erase(?CURRENT_CALL)
+  end.
 
 -spec handle_exception(CallerPid::pid(), Mod::atom(), Func::atom(),
-                       Args::[any()], Class:: exit | error | throw,
-                       Reason::any()) ->
-        no_return().
-handle_exception(Pid, Mod, Func, Args, Class, Reason) ->
-    case meck_ret_spec:is_meck_exception(Reason) of
-        {true, MockedClass, MockedReason} ->
-            raise(Pid, Mod, Func, Args, MockedClass, MockedReason);
-        _ ->
-            meck_proc:invalidate(Mod),
-            raise(Pid, Mod, Func, Args, Class, Reason)
-    end.
+    Args::[any()], Class:: exit | error | throw,
+    Reason::any(),
+    Stack::list()) ->
+  no_return().
+handle_exception(Pid, Mod, Func, Args, Class, Reason, Stack) ->
+  case meck_ret_spec:is_meck_exception(Reason) of
+    {true, MockedClass, MockedReason} ->
+      raise(Pid, Mod, Func, Args, MockedClass, MockedReason, Stack);
+    _ ->
+      meck_proc:invalidate(Mod),
+      raise(Pid, Mod, Func, Args, Class, Reason, Stack)
+  end.
 
 -spec raise(CallerPid::pid(), Mod::atom(), Func::atom(), Args::[any()],
-            Class:: exit | error | throw, Reason::any()) ->
-        no_return().
-raise(Pid, Mod, Func, Args, Class, Reason) ->
-    StackTrace = inject(Mod, Func, Args, erlang:get_stacktrace()),
-    meck_proc:add_history_exception(Mod, Pid, Func, Args,
-                                    {Class, Reason, StackTrace}),
-    erlang:raise(Class, Reason, StackTrace).
+    Class:: exit | error | throw, Reason::any(), Stack::list()) ->
+  no_return().
+raise(Pid, Mod, Func, Args, Class, Reason, Stack) ->
+  StackTrace = inject(Mod, Func, Args, Stack),
+  meck_proc:add_history_exception(Mod, Pid, Func, Args,
+    {Class, Reason, StackTrace}),
+  erlang:raise(Class, Reason, StackTrace).
 
 -spec inject(Mod::atom(), Func::atom(), Args::[any()],
-             meck_history:stack_trace()) ->
-        NewStackTrace::meck_history:stack_trace().
-inject(_Mod, _Func, _Args, []) ->
-    [];
+    meck_history:stack_trace()) ->
+  NewStackTrace::meck_history:stack_trace().
+inject(Mod, Func, Args, []) ->
+  [{Mod, Func, Args}];
 inject(Mod, Func, Args, [{?MODULE, exec, _AriOrArgs, _Loc}|Stack]) ->
-    [{Mod, Func, Args} | Stack];
+  [{Mod, Func, Args} | Stack];
 inject(Mod, Func, Args, [{?MODULE, exec, _AriOrArgs}|Stack]) ->
-    [{Mod, Func, Args} | Stack];
+  [{Mod, Func, Args} | Stack];
 inject(Mod, Func, Args, [Call|Stack]) when element(1, Call) == ?MODULE ->
-    inject(Mod, Func, Args, Stack);
+  inject(Mod, Func, Args, Stack);
 inject(Mod, Func, Args, [H | Stack]) ->
-    [H | inject(Mod, Func, Args, Stack)].
+  [H | inject(Mod, Func, Args, Stack)].
